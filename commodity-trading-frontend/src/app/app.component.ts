d6beeed01b6422fc91517eef17fa55a193ce2227
ananya-mishra-sp <ananya.mishra@spglobal.com>
2025-03-03 17:46:25 +0530

    Backend Initiated

--- commodity-trading-frontend/src/app/app.component.ts
+++ commodity-trading-frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ commodity/src/test/java/com/trading/commodity/CommodityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.commodity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommodityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
