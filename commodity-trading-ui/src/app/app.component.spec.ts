e2b05f9a149937a74b6add4cbaf362fe039dacb0
ananya-mishra-sp <ananya.mishra@spglobal.com>
2025-03-04 07:59:14 +0530

    Project Reinitiated

--- commodity-trading-ui/src/app/app.component.spec.ts
+++ commodity-trading-ui/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'commodity-trading-frontend' title`, () => {
+  it(`should have the 'commodity-trading-ui' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('commodity-trading-frontend');
+    expect(app.title).toEqual('commodity-trading-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, commodity-trading-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, commodity-trading-ui');
   });
 });
